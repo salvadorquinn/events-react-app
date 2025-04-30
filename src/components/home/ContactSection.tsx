4209beb46af9744f15312f45f2726868211a86cb
salvadorquinn <salvadorquinn2049@gmail.com>
2025-04-30 12:35:03 +0600

    Your commit message

---
+++ src/components/home/ContactSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactSection: React.FC = () => {
+    return (
+        <div>
+            <h2>Contact Section</h2>
+        </div>
+    );
+};
+
+export default ContactSection;
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,14 @@
+// Header 
+const Header = () => {
+    return (
+<nav className="navbar navbar-light bg-light">
+<div className="container">
+  <a className="navbar-brand" href="index.html">
+    <img src="studynet.png" alt="studynet" />
+  </a>
+</div>
+</nav>
+);
+};
+
+export default Header;
